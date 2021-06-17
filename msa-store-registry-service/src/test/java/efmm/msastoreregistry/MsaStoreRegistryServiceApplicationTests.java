adb4e881ce5ea4231e58d9a8f81203c7fc201ad7
edfermtz <54335086+edfermtzmtz@users.noreply.github.com>
2021-06-16 23:55:19 -0600

    ED-MS-02 Subiendo servidor de eureka y su configuracion

---
+++ msa-store-registry-service/src/test/java/efmm/msastoreregistry/MsaStoreRegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package efmm.msastoreregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaStoreRegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
