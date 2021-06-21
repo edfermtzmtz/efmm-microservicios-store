14b3ffe309ce0b8c9329bb6969f6e91a1970a43e
edfermtz <54335086+edfermtzmtz@users.noreply.github.com>
2021-06-21 12:57:24 -0600

    EF-MS-DT-001 Implementando spring security en discovery

--- msa-store-discovery/src/main/java/efmm/msa/store/discovery/MsaStoreDiscoveryApplication.java
+++ msa-store-discovery/src/main/java/efmm/msa/store/discovery/MsaStoreDiscoveryApplication.java
@@ -3,6 +3,9 @@ package efmm.msa.store.discovery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -10,6 +13,16 @@ public class MsaStoreDiscoveryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsaStoreDiscoveryApplication.class, args);
+	}
+	
+	@EnableWebSecurity
+	static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().ignoringAntMatchers("/eureka/**");
+			super.configure(http);
+		}
 	}
 
 }
